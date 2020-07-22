4493f3670b8f52630eaf59fa0620de9d033bdb39
Jose Manuel Martinez <2015378@ce.pucmm.edu.do>
2020-07-22 11:58:49 -0400

    Creando proyecto base Spring

---
+++ src/main/java/com/pucmm/arbolgenealogicoprolog/ArbolGenealogicoPrologApplication.java
@@ -0,0 +1,13 @@
+package com.pucmm.arbolgenealogicoprolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbolGenealogicoPrologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArbolGenealogicoPrologApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pucmm/arbolgenealogicoprolog/ArbolGenealogicoPrologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucmm.arbolgenealogicoprolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbolGenealogicoPrologApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
